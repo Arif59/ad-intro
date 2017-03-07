e1e793e3684e9f87fed9948f5bdc5cd39c2ee1cf
Ariful Islam <ariful.potato@gmail.com>
2017-03-07 21:33:14 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+	$('.player').trigger("pause");
+	$(".first").typed({
+	  strings: ["Every day comes with 86,400 seconds ^1000", "^500 Tick tock"],
+	  typeSpeed: 0,
+	  backSpeed: -10,
+	  startDelay: 1500
+	});
+	setTimeout(function() {
+    $('.player').trigger("play");
+
+  }, 1500);
+  setTimeout(function() {
+  	$('.player').trigger("pause");
+  }, 3000);
+  setTimeout(function() {
+  	$('.player').trigger("play");
+  }, 5800);
+  setTimeout(function() {
+  	$('.player').trigger("pause");
+  }, 6500);
+});
+
+
+
