88900ca8f975c40c7ef827c19911a2a8e6dfc713
Ariful Islam <ariful.potato@gmail.com>
2017-03-10 00:52:45 +0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
 	  strings: ["Every day comes with 86,400 seconds ^1000", "^500 Tick tock ^1500", ""],
 	  typeSpeed: 0,
 	  backSpeed: -10,
-	  startDelay: 1500
+	  startDelay: 5000
 	});
 // 	setTimeout(function() {
 //     	$('.typed-cursor').fadeOut(500);
